b3eb7d8f6812327299740725c4a0662f3fe085ca
Jerrified <oyedelejeremiah.ng@gmail.com>
2024-07-31 21:52:50 +0100

    Add functionality to add new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Todos from "./components/Todos";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import { TodoProps } from "./utils/todos";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [todos, setTodos] = useState<TodoProps[]>(() => {
+        const storedTodos = localStorage.getItem('todos');
+        return storedTodos ? JSON.parse(storedTodos) : [];
+      });
+    
+      useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+      }, [todos]); 
+  
+    const handleCheck = (id: number) => {
+      setTodos((pv) =>
+        pv.map((t) => (t.id === id ? { ...t, checked: !t.checked } : t))
+      );
+    };
+  
+    const removeElement = (id: number) => {
+      setTodos((pv) => pv.filter((t) => t.id !== id));
+    };
+  
+    return (
+      <section
+        className="min-h-screen bg-zinc-950 py-24"
+        style={{
+          backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='2' stroke='%2318181b'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
+        }}
+      >
+        <div className="mx-auto w-full max-w-xl px-4">
+          <Header />
+          <Todos
+            removeElement={removeElement}
+            todos={todos}
+            handleCheck={handleCheck}
+          />
+        </div>
+        <Form setTodos={setTodos} />
+      </section>
+    );
+  };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+  export default App
